19e40f20a673b3105b8216ee55565f91fa815bf1
Lucas <lucaswoodyard@yahoo.com>
2023-05-24 21:00:51 -0500

    Ran first migration and created a dogs and shelters table.

--- db/migrate/20230524220534_dog_shelter.rb
+++ db/migrate/20230524220534_dog_shelter.rb
@@ -1,4 +1,11 @@
 class DogShelter < ActiveRecord::Migration[6.1]
   def change
+    create_table :shelters do |t|
+      t.string :name 
+      t.string :image
+      t.string :location
+      t.integer :employees
+      t.string :description
+    end
   end
 end
--- db/migrate/20230524224015_all_dogs.rb
+++ db/migrate/20230524224015_all_dogs.rb
@@ -1,4 +1,12 @@
 class AllDogs < ActiveRecord::Migration[6.1]
   def change
+    create_table :dogs do |t|
+      t.string :dog_name
+      t.string :breed
+      t.integer :age
+      t.string :description
+      t.string :dog_image
+      t.integer :shelter_id
+    end
   end
 end
